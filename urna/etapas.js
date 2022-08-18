51d0b5407ea474aa2a03e1e45f8db7f51f501931
leoalvesdev99 <108308925+leoalvesdev99@users.noreply.github.com>
2022-08-18 20:07:35 -0300

    Add files via upload

---
+++ urna/etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Guilherme',
+                partido: 'AMOR',
+                fotos: [
+                    { url: 'man1.png', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Ludymilla',
+                partido: 'AMG',
+                fotos: [
+                    { url: 'woman.png', legenda: 'Vereadora' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Sabrina',
+                partido: 'AMOR',
+                vice: 'alguem',
+                fotos: [
+                    { url: 'woman.png', legenda: 'Prefeita' }
+                ]
+            },
+            {
+                numero: '21',
+                nome: 'Bernardo',
+                partido: 'AMG',
+                vice: 'algueem',
+                fotos: [
+                    { url: 'man.png', legenda: 'Prefeita' }
+                ]
+            },
+        ]
+    }
+]
